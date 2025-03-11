6a0aa140f00805c22b06fdba9ca4e4507a29641b
Adham Emam <mrado372002@gmail.com>
2025-03-11 02:54:28 +0200

    fix animation

--- components/ui/bg-animation.tsx
+++ components/ui/bg-animation.tsx
@@ -4,58 +4,30 @@ import { useTheme } from "next-themes";
 export const BackgroundAnimation = () => {
   const { resolvedTheme } = useTheme();
 
+  // Configuration for each circle
+  const circlesConfig = [
+    { id: 1, opacity: "opacity-20" },
+    { id: 2, opacity: "opacity-20" },
+    { id: 3, opacity: "opacity-20" },
+    { id: 4, opacity: "opacity-20" },
+    { id: 5, opacity: "opacity-20" },
+    { id: 6, opacity: "opacity-30" },
+    { id: 7, opacity: "opacity-30" },
+    { id: 8, opacity: "opacity-30" },
+    { id: 9, opacity: "opacity-30" },
+    { id: 10, opacity: "opacity-30" },
+  ];
+
   return (
     <ul className="circles absolute top-0 left-0 w-full h-full">
-      <li
-        className={`absolute block list-none w-5 h-5 opacity-20 ${
-          resolvedTheme === "dark" ? "bg-white" : "bg-black"
-        }`}
-      ></li>
-      <li
-        className={`absolute block list-none w-5 h-5 opacity-20 ${
-          resolvedTheme === "dark" ? "bg-white" : "bg-black"
-        }`}
-      ></li>
-      <li
-        className={`absolute block list-none w-5 h-5 opacity-20 ${
-          resolvedTheme === "dark" ? "bg-white" : "bg-black"
-        }`}
-      ></li>
-      <li
-        className={`absolute block list-none w-5 h-5 opacity-20 ${
-          resolvedTheme === "dark" ? "bg-white" : "bg-black"
-        }`}
-      ></li>
-      <li
-        className={`absolute block list-none w-5 h-5 opacity-20 ${
-          resolvedTheme === "dark" ? "bg-white" : "bg-black"
-        }`}
-      ></li>
-      <li
-        className={`absolute block list-none w-5 h-5 opacity-30 ${
-          resolvedTheme === "dark" ? "bg-white" : "bg-black"
-        }`}
-      ></li>
-      <li
-        className={`absolute block list-none w-5 h-5 opacity-30 ${
-          resolvedTheme === "dark" ? "bg-white" : "bg-black"
-        }`}
-      ></li>
-      <li
-        className={`absolute block list-none w-5 h-5 opacity-30 ${
-          resolvedTheme === "dark" ? "bg-white" : "bg-black"
-        }`}
-      ></li>
-      <li
-        className={`absolute block list-none w-5 h-5 opacity-30 ${
-          resolvedTheme === "dark" ? "bg-white" : "bg-black"
-        }`}
-      ></li>
-      <li
-        className={`absolute block list-none w-5 h-5 opacity-30 ${
-          resolvedTheme === "dark" ? "bg-white" : "bg-black"
-        }`}
-      ></li>
+      {circlesConfig.map((circle) => (
+        <li
+          key={circle.id}
+          className={`absolute block list-none ${circle.opacity} ${
+            resolvedTheme === "dark" ? "bg-white" : "bg-black"
+          }`}
+        ></li>
+      ))}
     </ul>
   );
 };
