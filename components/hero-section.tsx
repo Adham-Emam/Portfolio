63f091c1f8db595bca96b5a16df67723e447bf3c
Adham Emam <mrado372002@gmail.com>
2025-03-11 03:37:56 +0200

    rename resume

--- components/hero-section.tsx
+++ components/hero-section.tsx
@@ -58,8 +58,8 @@ export function HeroSection() {
           </Button>
           <a
             className="flex items-center justify-center text-muted-foreground bg-muted/50 hover:bg-muted/30 rounded-md px-4 py-2"
-            href="/AdhamEmam.pdf"
-            download="AdhamEmam.pdf"
+            href="/adham-emam.pdf"
+            download="adham-emam.pdf"
           >
             <Download className="mr-2 h-4 w-4" />
             Download Resume
