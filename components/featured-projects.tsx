52af0d5da740571dd6c085cfc855ced440cbab5d
Adham Emam <mrado372002@gmail.com>
2025-04-21 12:46:42 +0200

    add img priority

--- components/featured-projects.tsx
+++ components/featured-projects.tsx
@@ -47,7 +47,7 @@ export function FeaturedProjects() {
                   fill
                   sizes="(max-width: 768px) 100vw, (max-width: 1200px) 50vw, 33vw"
                   className="object-cover"
-                  priority={true}
+                  priority
                 />
               </div>
               <CardHeader>
