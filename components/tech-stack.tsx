2f36d0aa4f4bf92f7271c1964dc5f8acfb168fd3
Adham-Emam <mrado372002@gmail.com>
2026-03-30 09:49:17 +0200

    fix techstack bug

--- components/tech-stack.tsx
+++ components/tech-stack.tsx
@@ -48,7 +48,7 @@ export function TechStack() {
             key={cat}
             variant={filter === cat ? 'default' : 'outline'}
             className="cursor-pointer px-4 py-2"
-            onClick={() => (filter ? setFilter(null) : setFilter(cat))}
+            onClick={() => (filter === cat ? setFilter(null) : setFilter(cat))}
           >
             {cat.charAt(0).toUpperCase() + cat.slice(1)}
           </Badge>
